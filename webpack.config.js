bca057b6a2ae0bd22600f5fee57e357792a284cc
Tyler Wigington <tylerwigington@u.boisestate.edu>
2018-08-28 21:38:45 -0600

    wip webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const bundleOutputDir = './wwwroot/dist';
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env) => {
     const isDevBuild = !(env && env.prod);
     return [{
+        mode: isDevBuild ? 'development' : 'production',
         stats: { modules: false },
         entry: { 'main': './ClientApp/boot-app.js' },
         resolve: {
@@ -23,19 +26,16 @@ module.exports = (env) => {
             filename: '[name].js',
             publicPath: '/dist/'
         },
-        module: {
-            rules: [
-                { test: /\.vue$/, include: [/ClientApp/, path.resolve(__dirname, 'node_modules/vue-full-calendar')], use: 'vue-loader' },
-                { test: /\.js$/, include: /ClientApp/, use: 'babel-loader' },
-                { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader' }) },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
-            ]
-        },
         plugins: [
             new webpack.DllReferencePlugin({
                 context: __dirname,
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
-            })
+            }),
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'site.css',
+                chunkFilename: '[id].css'
+            }),
         ].concat(isDevBuild ? [
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
@@ -45,7 +45,35 @@ module.exports = (env) => {
         ] : [
                 // Plugins that apply in production builds only
                 new webpack.optimize.UglifyJsPlugin(),
-                new ExtractTextPlugin('site.css')
-            ])
+                // new ExtractTextPlugin('site.css')
+        ]),
+        module: {
+            rules: [
+                { 
+                    test: /\.vue$/, include: [/ClientApp/,path.resolve(__dirname, 'node_modules/vue-full-calendar')],
+                    use: 'vue-loader'
+                },
+                {
+                    test: /\.js$/,
+                    include: /ClientApp/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.css$/,
+                    use: isDevBuild ? 
+                    ['style-loader', 'css-loader'] :
+                    [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    use: 'url-loader?limit=25000'
+                }
+            ]
+        }
     }];
 };
